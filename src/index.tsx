2bf2e0396d5119a28b39991f69656036664afa19
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-03 18:05:17 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,117 +1,64 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import { useObservable } from "rxjs-hooks"
-import { map, filter, scan, bufferTime, tap } from "rxjs/operators"
-import { fromEvent, pipe } from "rxjs"
-import { Shake } from "reshake"
+import { useObservable, useEventCallback } from "rxjs-hooks"
+import {
+  map,
+  filter,
+  scan,
+  bufferTime,
+  tap,
+  debounceTime,
+  mergeMap,
+  combineAll,
+  startWith,
+  distinctUntilChanged
+} from "rxjs/operators"
+import { fromEvent, pipe, combineLatest, zip } from "rxjs"
+import { useState } from "react"
+import { searchApi } from "./api"
 
-const arrowMap = {
-  ArrowUp: "↑",
-  ArrowDown: "↓",
-  ArrowLeft: "←",
-  ArrowRight: "→"
-}
-
-const convertKeyMap = (keys: number[]) => {
-  const join = keys.join("_")
-  switch (join) {
-    case "37_38":
-      return "↖"
-    case "37_40":
-      return "↙"
-    case "38_39":
-      return "↗"
-    case "39_40":
-      return "↘"
-  }
-  return null
-}
-
-const convertVisible = (key) => {
-  if (arrowMap[key]) return arrowMap[key]
-  if (key.length === 1) return key.toUpperCase()
-  return null
-}
-
-const SucceedCommand = ({ succeedCommand }) => {
+const Search = ({ word, result, changeInput }) => {
   return (
-    <Shake
-      h={74}
-      v={80}
-      r={0}
-      dur={130}
-      int={6.3}
-      max={100}
-      fixed={true}
-      fixedStop={false}
-      freez={false}
-    >
-      <h1>{succeedCommand} Succeed</h1>
-    </Shake>
+    <div>
+      <input value={word} onChange={(e) => changeInput(e.target.value)} />
+      <h3>Result</h3>
+      <ul>
+        {result.map((r, i) => (
+          <li key={i}>{r}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-const convertKeys = (keys) =>
-  keys.length === 1
-    ? convertVisible(keys[0].key)
-    : convertKeyMap(keys.map(({ keyCode }) => keyCode).sort())
-
-type KeyTuple = { key: string; keyCode: number }
-
-const keyEventStream = (length) => {
-  return pipe(
-    map<KeyboardEvent, KeyTuple>(({ key, keyCode }) => ({ key, keyCode })),
-    tap((k) => console.log("key", k)),
-    bufferTime(60),
-    filter((item) => item.length > 0),
-    map((keys) => convertKeys(keys)),
-    filter((value) => !!value),
-    scan<string>((curr, next) => [...curr, next], []),
-    map((cmd: string[]) => cmd.slice(-1 * length))
-  )
-}
-
-export const useKeyCommnads = (length: number) => {
-  return useObservable(
-    () =>
-      fromEvent<KeyboardEvent>(document, "keydown").pipe(
-        keyEventStream(length)
-      ),
-    []
-  )
-}
-
-const isCommandSucceed = (keyeventLog: string[], command: string[]) => {
-  const current = keyeventLog.slice(-1 * command.length)
-  return command.join("") === current.join("")
-}
-
-export const useCommands = () => {
-  const commands = {
-    konami: ["↑", "↑", "↓", "↓", "←", "→", "←", "→", "B", "A"],
-    shoryuken: ["↓", "→", "↘", "P"],
-    hadoken: ["↓", "↘", "→", "P"]
-  }
-  const saveLength = Math.max(...Object.values(commands).map((l) => l.length))
-  const keyeventLog = useKeyCommnads(saveLength)
-  const succeedCommands = Object.entries(commands)
-    .filter(([_, command]) => {
-      return isCommandSucceed(keyeventLog, command)
-    })
-    .map(([name, commands]) => name)
-  return { keyeventLog, succeedCommands }
+type State = {
+  word: string
+  result: string[]
 }
 
 export const App = () => {
-  const { keyeventLog, succeedCommands } = useCommands()
+  const initial: [string, string[]] = ["", []] //{ word: "", result: [] }
+  const [keyboardCallack, [word, result]] = useEventCallback(
+    (event$) =>
+      combineLatest(
+        event$.pipe(
+          tap(console.log),
+          map((a) => a)
+        ),
+        event$.pipe(
+          startWith([]),
+          debounceTime(400),
+          distinctUntilChanged(),
+          mergeMap((word) => searchApi(word)),
+          tap(console.log),
+          map((result) => (result !== undefined ? result : []))
+        )
+      ),
+    initial
+  )
   return (
     <div>
-      <div>please key type: {keyeventLog}</div>
-      <div>
-        {succeedCommands.map((command) => (
-          <SucceedCommand succeedCommand={command} />
-        ))}
-      </div>
+      <Search changeInput={keyboardCallack} word={word} result={result} />
     </div>
   )
 }
